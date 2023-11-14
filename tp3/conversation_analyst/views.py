86e728c45feb50fde573c6d59863214d5e27aa8a
Simone <2827442l@student.gla.ac.uk>
2023-11-14 21:08:48 +0000

    fixed upload issues

--- tp3/conversation_analyst/views.py
+++ tp3/conversation_analyst/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .forms import UploadFileForm
+from .models import File
 
 
 # Create your views here.
 def homepage(request):
-    return render(request, "conversation_analyst/homepage.html")
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded = request.FILES["file"]
+            file_obj = File.objects.create(file=uploaded)
+            file_obj.save()
+            return HttpResponse("You have successfully uploaded" + str(uploaded))
+    else:
+        form = UploadFileForm()
+    return render(request, "conversation_analyst/homepage.html", {"form": form})
--- tp3/tp3/settings.py
+++ tp3/tp3/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'conversation_analyst',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uk'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
