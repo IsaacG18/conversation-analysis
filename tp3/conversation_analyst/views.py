2e07cbe79f41e133cb8b73ec8c71fa329fe75427
Isaac Gilbert <isaac.a.gilbert@gmail.com>
2023-11-23 18:43:47 +0000

    #53 add fixes to nlp pipeline

--- tp3/conversation_analyst/views.py
+++ tp3/conversation_analyst/views.py
@@ -48,10 +48,10 @@ def content_review(request):
     return render(request, "conversation_analyst/content_review.html", context=context_dict)
 
 def process_file(file_path, delimiters =[["Timestamp", ","], ["Sender", ":"]], keywords = Keywords()):
-    chat_messages = ingestion.parse_chat_file(file_path, delimiters)
+    chat_messages =  ingestion.parse_chat_file(file_path, delimiters)
     message_count = create_arrays(chat_messages)
-    nlp_text = tag_text(message_to_text(chat_messages), keywords)
+    nlp_text, Keywords = tag_text(message_to_text(chat_messages), keywords)
     person_and_locations = extract(nlp_text, ["PERSON", "GPE"])
-    risk_words = get_top_n_risk_keywords(nlp_text, 3)
-    common_topics = get_top_n_common_topics_with_avg_risk(nlp_text, 3)
+    risk_words = get_top_n_risk_keywords(Keywords, 3)
+    common_topics = get_top_n_common_topics_with_avg_risk(Keywords, 3)
     return chat_messages, message_count,person_and_locations,risk_words,common_topics
