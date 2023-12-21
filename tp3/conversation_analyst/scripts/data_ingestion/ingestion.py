5854577f5c3735c1ad92500da4f865594fbc873d
Platt Duangdee <2926767D@student.gla.ac.uk>
2023-12-21 13:27:02 +0000

    added parse_chat_file to accept csv

--- tp3/conversation_analyst/scripts/data_ingestion/ingestion.py
+++ tp3/conversation_analyst/scripts/data_ingestion/ingestion.py
@@ -1,4 +1,5 @@
 import re
+import csv
 from docx import Document
 
 def parse_chat_file(file_path, delimiters):
@@ -9,22 +10,35 @@ def parse_chat_file(file_path, delimiters):
     delimiters.append(["Message", ""])
     messages = []
 
-
     try:
         if file_path.endswith('.docx'):
             doc = Document(file_path)
             lines = [paragraph.text for paragraph in doc.paragraphs]
+            for line in lines:
+                match = re.match(pattern, line)
+                if match:
+                    values = match.groups()
+                    message_dict = {key: value for (key, _), value in zip(delimiters, values)}
+                    messages.append(message_dict)
+
+        elif file_path.endswith('.csv'):
+            with open(file_path, 'r') as file:
+                csv_reader = csv.reader(file)
+                headers = next(csv_reader, None)
+                for row in csv_reader:
+                    message_dict = {headers[i]: row[i] for i in range(len(headers))}
+                    messages.append(message_dict)
+
         else:
             with open(file_path, 'r') as file:
                 lines = file.readlines()
+            for line in lines:
+                match = re.match(pattern, line)
+                if match:
+                    values = match.groups()
+                    message_dict = {key: value for (key, _), value in zip(delimiters, values)}
+                    messages.append(message_dict)
 
-        for line in lines:
-            match = re.match(pattern, line)
-            if match:
-                values = match.groups()
-
-                message_dict = {key: value for (key, _), value in zip(delimiters, values)}
-                messages.append(message_dict)
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         return messages
