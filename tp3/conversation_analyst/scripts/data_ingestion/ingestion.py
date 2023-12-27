5d939dbb4c7e4efcd77634627561176afc007dbc
Isaac <2657998G@student.gla.ac.uk>
2023-12-27 21:23:58 +0000

    #44 make the ingestion match the dilinator format

--- tp3/conversation_analyst/scripts/data_ingestion/ingestion.py
+++ tp3/conversation_analyst/scripts/data_ingestion/ingestion.py
@@ -7,7 +7,9 @@ def parse_chat_file(file_path, delimiters):
     for key, delim in delimiters:
         pattern += f'(.*?){delim}\s'
     pattern += '(.*)$'
-    delimiters.append(["Message", ""])
+    d = list (delimiters)
+    d.append(["Message", ""])
+    
     messages = []
 
     try:
@@ -18,16 +20,19 @@ def parse_chat_file(file_path, delimiters):
                 match = re.match(pattern, line)
                 if match:
                     values = match.groups()
-                    message_dict = {key: value for (key, _), value in zip(delimiters, values)}
+                    message_dict = {key: value for (key, _), value in zip(d, values)}
                     messages.append(message_dict)
 
         elif file_path.endswith('.csv'):
             with open(file_path, 'r') as file:
+                
                 csv_reader = csv.reader(file)
-                headers = next(csv_reader, None)
+                next(csv_reader, None)
+                headers = [columns[0] for columns in d]
                 for row in csv_reader:
                     message_dict = {headers[i]: row[i] for i in range(len(headers))}
                     messages.append(message_dict)
+
 
         else:
             with open(file_path, 'r') as file:
@@ -36,12 +41,12 @@ def parse_chat_file(file_path, delimiters):
                 match = re.match(pattern, line)
                 if match:
                     values = match.groups()
-                    message_dict = {key: value for (key, _), value in zip(delimiters, values)}
+                    message_dict = {key: value for (key, _), value in zip(d, values)}
                     messages.append(message_dict)
 
     except FileNotFoundError:
         print(f"File not found: {file_path}")
     except Exception as e:
         print(f"An error occurred: {e}")
-
+    print(messages)
     return messages
