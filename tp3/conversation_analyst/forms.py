44c444e6d95197c42368d877755dbb3f945379a2
Anastasiia Kadykova <2829507k@student.gla.ac.uk>
2023-12-31 20:47:18 +0100

    #69 added delimiters to upload page, handling specific type of delims

--- tp3/conversation_analyst/forms.py
+++ tp3/conversation_analyst/forms.py
@@ -3,3 +3,20 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+    sender_delim = forms.CharField()
+    timestamp_delim = forms.CharField()
+    custom_delim = forms.CharField()
+
+    def clean_sender_delim(self):
+        cleaned_data = self.cleaned_data
+        delim = cleaned_data.get('sender_delim')
+        if delim.strip().isalnum():
+            raise form.ValidationError('Alphanumerical character can not be a delimiter.')
+        return delim
+    
+    def clean_timestamp_delim(self):
+        cleaned_data = self.cleaned_data
+        delim = cleaned_data.get('timestamp_delim')
+        if delim.strip().isalnum():
+            raise form.ValidationError('Alphanumerical character can not be a delimiter.')
+        return delim
