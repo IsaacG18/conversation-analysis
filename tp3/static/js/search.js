5092cabbbd4abb33b3e1a5acd467d0ecb8b57049
Simone <2827442l@student.gla.ac.uk>
2024-01-15 21:00:39 +0000

    #79 move search bar into navbar

--- tp3/static/js/search.js
+++ tp3/static/js/search.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-    $("#search-button").click(function () {
+    $("#search-button").click(function (e) {
+        e.preventDefault();
+        console.log("detected");
         var query = document.getElementById('search-input').value;
+        console.log(query);
         $.get(window.location.href,
                 {'query': query},
                 function(data) {
+                    console.log(data);
                     $('#results').replaceWith(data);
         });
     })
