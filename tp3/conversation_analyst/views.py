78465c91bf354f738ad3a224ba3e0617d6a5e05f
Isaac <2657998G@student.gla.ac.uk>
2024-01-19 14:46:37 +0000

    #80 add risk rating to view so that it adds to model

--- tp3/conversation_analyst/views.py
+++ tp3/conversation_analyst/views.py
@@ -122,7 +122,7 @@ def process_file(file, delimiters=[["Timestamp", ","], ["Sender", ":"]], keyword
     chat_messages = ingestion.parse_chat_file(file_path, delimiters)
     message_count = create_arrays(chat_messages)
     nlp_text, person_and_locations = tag_text(chat_messages, keywords, ["PERSON", "GPE"])
-    risk_words = get_top_n_risk_keywords(nlp_text, 3)
+    risk_words = get_top_n_risk_keywords(nlp_text, 10)
     print("risk words: ")
     print(risk_words)
     common_topics = get_top_n_common_topics_with_avg_risk(nlp_text, 3)
@@ -134,7 +134,8 @@ def generate_analysis_objects(file, chat_messages, message_count, person_and_loc
     locations = person_and_locations['GPE']
 
     for message in chat_messages:
-        m = add_message(file, message['Timestamp'], message['Sender'], message['Message'], message["Display_Message"])
+        print(message["risk"])
+        m = add_message(file, message['Timestamp'], message['Sender'], message['Message'], message["Display_Message"],  message["risk"])
     a = add_analysis(file)
     add_vis(a, plots(chat_messages, file.slug))
     for person in persons:
