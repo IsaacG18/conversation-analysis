dd13a1260ed2e52c103950ef0e357b458bbb6700
Simone <2827442l@student.gla.ac.uk>
2024-01-21 22:31:10 +0000

    #79 remove print statements, enable form submission

--- tp3/static/js/search.js
+++ tp3/static/js/search.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    $("#search-button").click(function (e) {
+    $("#search-bar").submit(function (e) {
         e.preventDefault();
-        console.log("detected");
         var query = document.getElementById('search-input').value;
         console.log(query);
         $.get(window.location.href,
                 {'query': query},
                 function(data) {
-                    console.log(data);
                     $('#results').replaceWith(data);
         });
     })
