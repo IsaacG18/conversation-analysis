9527acef28a8e45d66f0eb5e619246121fb930e4
Isaac <2657998G@student.gla.ac.uk>
2024-01-26 03:32:33 +0000

    #83 simiply the changes made in ingestion

--- tp3/conversation_analyst/scripts/data_ingestion/ingestion.py
+++ tp3/conversation_analyst/scripts/data_ingestion/ingestion.py
@@ -6,41 +6,14 @@ from conversation_analyst.models import DateFormat
 from datetime import datetime
 from dateutil import parser
 
-def get_date_formats():
-    formats = DateFormat.objects.all()
-    print([(fmt.name, re.compile(fmt.regex)) for fmt in formats])
-    return [(fmt.name, re.compile(fmt.regex)) for fmt in formats]
-
-def parse_specific_format(timestamp, format_name):
-    # Add custom parsing logic here:
-    if format_name == "ISO 8601":
-        return parser.parse(timestamp)
-
-    elif format_name == "Unix Timestamp":
-        return datetime.utcfromtimestamp(int(timestamp))
-
-    elif format_name == "Common Log Format":
-        match = re.search(r'\[([^:]+):(\d+:){2}\d+ ([^\]]+)\]', timestamp)
-        if match:
-            timestamp_str = match.group(1) + ' ' + match.group(3)
-            datetime_obj = datetime.strptime(timestamp_str, '%d/%b/%Y %H:%M:%S %z')
-            return datetime_obj
-        
-    elif format_name == "ISO 8601 Without Seconds":
-        return datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
-
-    else:
-        raise ValueError(f"Unrecognized format name: {format_name}")
-
-    return datetime_obj.isoformat()
 
 def parse_timestamp(timestamp, date_formats):
-    for name, regex in date_formats:
-        if regex.match(timestamp):
-            return parse_specific_format(timestamp, name).isoformat()
-    raise ValueError(f"Unrecognized timestamp format: {timestamp}")
+    if date_formats == "":
+        return datetime.datetime.fromtimestamp(date_formats)
+    else:
+        return datetime.strptime(timestamp, date_formats).strftime("%Y-%m-%d %H:%M:%S")
 
-def parse_chat_file(file_path, delimiters):
+def parse_chat_file(file_path, delimiters, date_formats="%Y-%m-%dT%H:%M:%S"):
     pattern = '^'
     for key, delim in delimiters:
         pattern += f'(.*?){delim}\\s'
@@ -51,7 +24,7 @@ def parse_chat_file(file_path, delimiters):
     messages = []
     lines = []
 
-    date_formats = get_date_formats()
+
 
     try:
         if file_path.endswith('.docx'):
@@ -69,6 +42,7 @@ def parse_chat_file(file_path, delimiters):
                     headers = [columns[0] for columns in d]
                     for row in csv_reader:
                         message_dict = {headers[i]: row[i] for i in range(len(headers))}
+                        message_dict["Timestamp"] = parse_timestamp(message_dict["Timestamp"], date_formats)
                         messages.append(message_dict)
             except Exception as e:
                 raise ValueError(f"Error reading CSV file: {e}")
