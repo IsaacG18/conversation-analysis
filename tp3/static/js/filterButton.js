12044ce6858b01101fb163f0e794fbe3408a161b
Isaac <2657998G@student.gla.ac.uk>
2024-01-30 00:15:41 +0000

    .#74 issues with merge request
    Was using an old content_review page that did not have updated message feature
    This lead to issues with filter, this was not nocticed before because the filter.js was also out of date, both have been fixed

--- tp3/static/js/filterButton.js
+++ tp3/static/js/filterButton.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var startDate = document.getElementById('startDate').value;
     var endDate = document.getElementById('endDate').value;
+    var risk = []
 
     $(".date-fil").click(function () {
         startDate = document.getElementById('startDate').value;
@@ -8,19 +9,53 @@ $(document).ready(function () {
         var button = document.getElementsByClassName("existing");
         var filter_vals = []
         var pageSlug = window.location.pathname.split('/').pop(); 
-        var clickedElement = this; 
         for (var i = 0; i < button.length; i++) {
             var buttonText = button[i].textContent || button[i].innerText;
-            filter_vals.push(buttonText)
+            filter_vals.push(removeNumberedSuffix(buttonText))
         }
 
         $.get('/filter/',
-                {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug},
+                {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug,"risk":JSON.stringify(risk)},
                 function(data) {
-                    var messageOutputDiv = document.getElementById("message_output");
-                    messageOutputDiv.innerHTML = formatMessagesHTML(extractMessageDetails(data));
+                    $('.results').replaceWith(data.results);
                 });
     })
+
+    $(".risk").click(function () {
+        var clickedElement = this; 
+        var button = document.getElementsByClassName("existing");
+        var filter_vals = []
+        var risk_value  = parseInt($(this).val())
+        
+        var pageSlug = window.location.pathname.split('/').pop(); 
+        for (var i = 0; i < button.length; i++) {
+            var buttonText = button[i].textContent || button[i].innerText;
+            filter_vals.push(removeNumberedSuffix(buttonText))
+        }
+        if($(this).hasClass("clicked")){
+            let index = risk.indexOf(risk_value);
+            if (index !== -1) {
+                risk.splice(index, 1);
+            }
+            $.get('/filter/',
+                {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug, "risk":JSON.stringify(risk)},
+                function(data) {
+                    $('.results').replaceWith(data.results);
+                    $(clickedElement).removeClass("clicked");
+                });
+        }else{
+            risk.push(risk_value)
+            $.get('/filter/',
+                {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug, "risk":JSON.stringify(risk)},
+                function(data) {
+                    $('.results').replaceWith(data.results);
+                    $(clickedElement).addClass("clicked");
+                });
+        }
+    })
+
+
+
     $(".filter").click(function () {
         var button = document.getElementsByClassName("existing");
         var filter_vals = []
@@ -30,93 +65,43 @@ $(document).ready(function () {
         if (!$(this).hasClass("existing")) {
             for (var i = 0; i < button.length; i++) {
                 var buttonText = button[i].textContent || button[i].innerText;
-                filter_vals.push(buttonText)
+                filter_vals.push(removeNumberedSuffix(buttonText))
             }
 
-            filter_vals.push(buttonValue);
+            filter_vals.push(removeNumberedSuffix(buttonValue));
 
             $.get('/filter/',
-            {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug},
+            {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug,"risk":JSON.stringify(risk)},
                 function(data) {
                     $(clickedElement).addClass("existing");
                     $(clickedElement).addClass("clicked");
-                    var messageOutputDiv = document.getElementById("message_output");
-                    messageOutputDiv.innerHTML = formatMessagesHTML(extractMessageDetails(data));
+                    console.log(data.results);
+                    $('.results').replaceWith(data.results);
                 });
         }else{
     
             for (var i = 0; i < button.length; i++) {
                 var buttonText = button[i].textContent || button[i].innerText;
                 if (buttonText != buttonValue){
-                    filter_vals.push(buttonText)
+                    filter_vals.push(removeNumberedSuffix(buttonText))
                 }
                     
             }
     
             $.get('/filter/',
-            {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug},
+            {'filters': JSON.stringify(filter_vals), 'startDate':startDate, 'endDate':endDate ,'file_slug': pageSlug, "risk":JSON.stringify(risk)},
                 function(data) {
                     $(clickedElement).removeClass("existing");
                     $(clickedElement).removeClass("clicked");
-                    var messageOutputDiv = document.getElementById("message_output");
-                    messageOutputDiv.innerHTML = formatMessagesHTML(extractMessageDetails(data));
+                    $('.results').replaceWith(data.results);
                 });
         }
         
     });
 });
 
-
-function extractMessageDetails(data) {
-    try {
-        // Parse the "messages" property into an array of objects
-        const messages = JSON.parse(data.messages);
-
-        // Extract details from each message
-        const messageDetailsList = messages.map(item => {
-            return {
-                sender: item.fields.sender,
-                timestamp: item.fields.timestamp,
-                display_content: item.fields.display_content
-            };
-        });
-
-        return messageDetailsList;
-    } catch (error) {
-        console.error(`Error decoding JSON: ${error.message}`);
-        return [];
-    }
-}
-
-function formatMessagesHTML(messageDetails) {
-    if (messageDetails.length > 0) {
-        let html = '<div class="list-group">';
-        messageDetails.forEach(message => {
-            // Format timestamp using JavaScript Date object
-            const formattedTimestamp = new Date(message.timestamp).toLocaleString('en-US', {
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric',
-                hour12: true
-            });
-
-            html += `
-                <div class="list-group-item">
-                    <div class="d-flex justify-content-between">
-                        <span class="sender"><small>${message.sender}:</small></span>
-                        <small class="timestamp text-body-secondary">${formattedTimestamp}</small>
-                    </div>
-                    <small class="content">${message.display_content}</small>
-                </div>`;
-        });
-
-        html += '</div>';
-        return html;
-    } else {
-        return '<strong>There are no messages present.</strong>';
-    }
+function removeNumberedSuffix(input) {
+    return input.replace(/\(\d+\)$/, '');
 }
 var styles = `
   <style>
