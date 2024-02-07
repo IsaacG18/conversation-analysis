affe8ebd0a87042e8d8665bed22debeffae20300
Isaac <2657998G@student.gla.ac.uk>
2024-02-07 17:55:00 +0000

    #99 reverted issues in code issue 84
    Add the removed code in merge request 84 that got removed in the merge
    requesy of 97

--- tp3/conversation_analyst/models.py
+++ tp3/conversation_analyst/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-from pathlib import Path
-
+from .scripts.nlp.nlp import get_keyword_lamma
 
 
 # Create your models here.
@@ -22,22 +21,6 @@ class File(models.Model):
     def __str__(self):
         return self.title
 
-
-class Message(models.Model):
-    file = models.ForeignKey(File, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField()
-    sender = models.CharField(max_length=50)
-    main_sender = models.CharField(max_length=50)
-    content = models.CharField(max_length=1000)
-    display_content = models.CharField(max_length=1100)
-    risk_rating = models.IntegerField(default=0)
-
-    def set_main_sender(self, new_sender):
-        self.main_sender = new_sender
-        self.save()
-    def __str__(self):
-        return self.sender + self.timestamp.__str__()
-
 class KeywordPlan(models.Model):
     name = models.CharField(max_length=128, null=True)
     def __str__(self):
@@ -50,6 +33,23 @@ class Analysis(models.Model):
         verbose_name_plural = "Analyses"
     def __str__(self):
         return self.file.__str__()
+
+
+class Message(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    sender = models.CharField(max_length=50)
+    main_sender = models.CharField(max_length=50)
+    content = models.CharField(max_length=1000)
+    display_content = models.CharField(max_length=1100)
+    risk_rating = models.IntegerField(default=0)
+    tags = models.CharField(max_length=1024)
+
+    def set_main_sender(self, new_sender):
+        self.main_sender = new_sender
+        self.save()
+    def __str__(self):
+        return self.sender + self.timestamp.__str__()
 
 
 class Person(models.Model):
@@ -95,12 +95,13 @@ class Topic(models.Model):
 class RiskWord(models.Model):
     suite = models.ForeignKey(KeywordSuite, on_delete= models.CASCADE)
     topics = models.ManyToManyField(Topic, blank=True)
-    keyword = models.CharField(max_length=50)
-    risk_factor = models.IntegerField(default=0)
+    keyword = models.CharField(max_length=128)
+    risk_factor = models.IntegerField(default=128)
     amount = models.IntegerField(default=0)
-    
+    lemma = models.CharField(max_length=128)
+
     def save(self, *args, **kwargs):
-        self.keyword = self.keyword.lower()
+        self.lemma = get_keyword_lamma(self.keyword)
         super(RiskWord, self).save(*args, **kwargs)
 
     def __str__(self):
--- tp3/conversation_analyst/views.py
+++ tp3/conversation_analyst/views.py
@@ -117,7 +117,7 @@ def filter_view(request):
             filter_condition = Q()
             for word in filters:
                 search_term = r'(?<![a-zA-Z0-9]){}(?![a-zA-Z0-9])'.format(word)
-                filter_condition |= Q(content__iregex=search_term)
+                filter_condition |= Q(tags__iregex=search_term)
             messages = messages.filter(filter_condition)
         
         if len(risk)> 0:
@@ -356,7 +356,7 @@ def message(request):
         conversation_history.append( {"role": message.typeOfMessage, "content": message.content})
 
     client = OpenAI(
-    api_key="",
+    api_key="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",
     )
     
 
