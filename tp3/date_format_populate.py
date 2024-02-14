4bcee9298e9bde577d340950d6528baf59575135
Isaac <2657998G@student.gla.ac.uk>
2024-02-14 17:51:06 +0000

    Revert " #103 update the build script and begin to lint"
    This reverts commit 293938a81855c9417b90dace31f20dec4072532a.

--- tp3/date_format_populate.py
+++ tp3/date_format_populate.py
@@ -1,51 +1,27 @@
 import os
 from os.path import isfile, join
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tp3.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'tp3.settings')
 import django
 
 django.setup()
 
 from conversation_analyst.scripts.data_ingestion import ingestion
-from conversation_analyst.scripts.object_creators import add_delim, add_date
+from conversation_analyst.scripts.nlp.nlp import *
+from conversation_analyst.scripts.object_creators import *
 
 
 def populate_dates():
     date_formats = [
-        {
-            "name": "ISO 8601",
-            "example": "2022-01-24T12:34:56",
-            "format": "%Y-%m-%dT%H:%M:%S",
-            "default": True,
-        },
-        {
-            "name": "Unix Timestamp",
-            "example": "1643057699",
-            "format": r"^\d{10}(?:\.\d+)?$",
-            "default": False,
-        },
-        {
-            "name": "Common Log Format",
-            "example": "01/Feb/2024:12:34:56 +0000",
-            "format": "%d/%b/%Y:%H:%M:%S %z",
-            "default": False,
-        },
-        {
-            "name": "ISO 8601 Without Seconds",
-            "example": "2022-03-05 17:30",
-            "format": "%Y-%m-%d %H:%M",
-            "default": False,
-        },
+        {"name": "ISO 8601", "example": "2022-01-24T12:34:56", "format": "%Y-%m-%dT%H:%M:%S", "default": True},
+        {"name": "Unix Timestamp", "example": "1643057699", "format": r'^\d{10}(?:\.\d+)?$', "default": False},
+        {"name": "Common Log Format", "example": '01/Feb/2024:12:34:56 +0000', "format": "%d/%b/%Y:%H:%M:%S %z", "default": False},
+        {"name": "ISO 8601 Without Seconds", "example": "2022-03-05 17:30", "format":"%Y-%m-%d %H:%M", "default": False}
     ]
 
     for date_format_data in date_formats:
-        add_date(
-            date_format_data["name"],
-            date_format_data["example"],
-            date_format_data["format"],
-            date_format_data["default"],
-        )
-
+        add_date(date_format_data["name"], date_format_data["example"], date_format_data["format"], date_format_data["default"])
 
 def populate_delims():
     delimiters = [
@@ -57,8 +33,8 @@ def populate_delims():
         add_delim(delim["name"], delim["order"], delim["value"], delim["is_default"])
 
 
-if __name__ == "__main__":
-    print("Starting population script...")
+if __name__ == '__main__':
+    print('Starting population script...')
     populate_dates()
     populate_delims()
-    print("Finished")
+    print('Finished')
