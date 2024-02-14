548de8f0fb018ace79b765e4d5e8ac6a3a984797
Isaac <2657998G@student.gla.ac.uk>
2024-02-14 23:31:50 +0000

    #103 add README and added api key set up
    Added readme on how to run the code as well as used settings to set up
    an api key

--- tp3/tp3/settings.py
+++ tp3/tp3/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 import os
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,6 +22,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
